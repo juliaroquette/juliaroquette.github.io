21503b43493fef53133f28b0a728e3e01dabb2a6
Julia Roquette <juliaroquette@gmail.com>
2023-04-25 19:52:10 +0200

    FixNavBar
    Fix issue with sticky NavBar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $(window).on("load", function(){
-    $(".loader .inner").fadeOut(500, function(){
-      $(".loader").fadeOut(750);
-    });
+  $(".loader .inner").fadeOut(500, function(){
+    $(".loader").fadeOut(750);
   });
+});
+$(document).ready(function(){ //things starting with $ are jquery
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
+});
